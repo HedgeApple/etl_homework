832b0f4b730c5b9db28640ade305a9c8eb195140
Ian Daniel <66013003+IanDanielM@users.noreply.github.com>
2023-09-28 08:37:32 +0100

    Missing attributes

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 import logging
 import pandas as pd
-from map_cols import generate_column_mapping, format_ean13
+from map_cols import (generate_column_mapping, format_ean13,
+                      add_new_columns, column_order,
+                      country_mapping)
 
 
-country_mapping = {
-    'China': 'CHN',
-    'India': 'IND',
-    'Indonesia': 'IDN',
-    'Phillipines': 'PH',
-    'Thailand': 'TH',
-    'Vietnam': 'VN'
-}
-
-
-def process_file(filename: str, processed_file: str,
-                 n_boxes: int, n_bullets: int) -> None:
+def process_file(filename: str, processed_file: str) -> None:
     try:
         homework_df = pd.read_csv(filename, low_memory=False)
         # column mapping
-        column_mapping = generate_column_mapping(n_boxes=n_boxes,
-                                                 n_bullets=n_bullets)
+        column_mapping = generate_column_mapping(homework_df)
         homework_df.rename(columns=column_mapping, inplace=True)
-        homework_df = homework_df[column_mapping.values()]
-
+        new_columns = set(column_mapping.values())
+        if new_columns.issubset(homework_df.columns):
+            homework_df = homework_df[list(new_columns)]
+        else:
+            missing_columns = new_columns.difference(homework_df.columns)
+            logging.error("Warning: The following columns are missing:"
+                          f"{missing_columns}")
+        homework_df = add_new_columns(homework_df)
         # converting ean to str and matching to the format in example.csv
         homework_df['ean13'] = homework_df['ean13'].apply(format_ean13)
         homework_df['product__country_of_origin__alpha_3'] = homework_df[
             'product__country_of_origin__alpha_3'].map(country_mapping)
-
         # removing $ sign from currency and putting it two decimal places
         homework_df['cost_price'] = homework_df['cost_price'].replace(
             {'\$': '', ',': ''}, regex=True).astype(float)
@@ -35,11 +30,12 @@ def process_file(filename: str, processed_file: str,
             {'\$': '', ',': ''}, regex=True).astype(float)
         homework_df['cost_price'] = homework_df['cost_price'].round(2)
         homework_df['min_price'] = homework_df['min_price'].round(2)
+        columns = column_order()
+        homework_df = homework_df[columns]
         homework_df.to_csv(processed_file, index=False)
     except Exception as err:
         logging.error(f"An error occurred: {err}")
 
 
 if __name__ == '__main__':
-    process_file("files/homework.csv", "files/formatted.csv",
-                 n_boxes=3, n_bullets=7)
+    process_file("files/homework.csv", "files/formatted9.csv")
--- map_cols.py
+++ map_cols.py
@@ -1,4 +1,7 @@
-def generate_column_mapping(n_boxes: int, n_bullets: int) -> dict[str, str]:
+import pandas as pd
+
+
+def generate_column_mapping(df: pd.DataFrame) -> dict[str, str]:
     column_mapping = {
         "item number": "manufacturer_sku",
         "upc": "ean13",
@@ -34,15 +37,18 @@ def generate_column_mapping(n_boxes: int, n_bullets: int) -> dict[str, str]:
         "item style": "product__styles"
     }
 
-    # Generate 'product__bullets' mappings
+    bullet_cols = [col for col in df.columns if 'selling point' in col]
+    n_bullets = len(bullet_cols)
     for i in range(n_bullets):
-        column_mapping[f"selling point {i+1}"] = f"product__bullets_{i}"
+        column_mapping[f"selling point {i+1}"] = f"product__bullets__{i}"
 
-    # Generate 'boxes' mappings
+    box_cols = [col for col in df.columns if 'carton' in col.split(' ')[0]]
+    n_boxes = len(set([col.split(' ')[1] for col in box_cols if len(col.split(' ')) > 1]))
     for i in range(n_boxes):
         for attribute in ["weight", "length", "height", "width"]:
             column_name = f"carton {i+1} {attribute} (pounds)" if attribute == "weight" else f"carton {i+1} {attribute} (inches)"
-            column_mapping[column_name] = f"boxes__{i}__{attribute}"
+            if column_name in df.columns:
+                column_mapping[column_name] = f"boxes__{i}__{attribute}"
 
     return column_mapping
 
@@ -50,9 +56,111 @@ def generate_column_mapping(n_boxes: int, n_bullets: int) -> dict[str, str]:
 def format_ean13(ean: int) -> str:
     try:
         if ean < 0:
-            return None
-        ean = str(int(ean)).zfill(12)
+            return ""
+        ean_str = str(int(ean)).zfill(12)
     except ValueError:
-        return None
-    ean13 = f"0{ean[:2]}-{ean[2:11]}-{ean[11]}"
+        return ""
+    ean13 = f"0{ean_str[:2]}-{ean_str[2:11]}-{ean_str[11]}"
     return ean13
+
+
+def add_new_columns(df: pd.DataFrame) -> pd.DataFrame:
+    new_columns = ["made_to_order", "product__configuration__codes",
+                   "product__parent_sku", "attrib__assembly_required",
+                   "attrib__back_material", "attrib__blade_finish",
+                   "attrib__design_id", "attrib__distressed_finish",
+                   "attrib__fill", "attrib__frame_color", "attrib__hardwire",
+                   "attrib__leg_color", "attrib__leg_finish",
+                   "attrib__orientation", "attrib__seat_depth",
+                   "attrib__seat_width", "attrib__shade", "attrib__size",
+                   "attrib__warranty_years", "attrib__weave"]
+
+    for column in new_columns:
+        df[column] = None
+    return df
+
+
+def column_order() -> list[str]:
+    columns = [
+        "manufacturer_sku",
+        "ean13",
+        "weight",
+        "length",
+        "width",
+        "height",
+        "cost_price",
+        "min_price",
+        "made_to_order",
+        "product__product_class__name",
+        "product__brand__name",
+        "product__title",
+        "product__description",
+        "product__bullets__0",
+        "product__bullets__1",
+        "product__bullets__2",
+        "product__bullets__3",
+        "product__bullets__4",
+        "product__bullets__5",
+        "product__bullets__6",
+        "product__configuration__codes",
+        "product__multipack_quantity",
+        "product__country_of_origin__alpha_3",
+        "product__parent_sku",
+        "attrib__arm_height",
+        "attrib__assembly_required",
+        "attrib__back_material",
+        "attrib__blade_finish",
+        "attrib__bulb_included",
+        "attrib__bulb_type",
+        "attrib__color",
+        "attrib__cord_length",
+        "attrib__design_id",
+        "attrib__designer",
+        "attrib__distressed_finish",
+        "attrib__fill",
+        "attrib__finish",
+        "attrib__frame_color",
+        "attrib__hardwire",
+        "attrib__kit",
+        "attrib__leg_color",
+        "attrib__leg_finish",
+        "attrib__material",
+        "attrib__number_bulbs",
+        "attrib__orientation",
+        "attrib__outdoor_safe",
+        "attrib__pile_height",
+        "attrib__seat_depth",
+        "attrib__seat_height",
+        "attrib__seat_width",
+        "attrib__shade",
+        "attrib__size",
+        "attrib__switch_type",
+        "attrib__ul_certified",
+        "attrib__warranty_years",
+        "attrib__wattage",
+        "attrib__weave",
+        "attrib__weight_capacity",
+        "boxes__0__weight",
+        "boxes__0__length",
+        "boxes__0__height",
+        "boxes__0__width",
+        "boxes__1__weight",
+        "boxes__1__length",
+        "boxes__1__height",
+        "boxes__1__width",
+        "boxes__2__weight",
+        "boxes__2__length",
+        "boxes__2__height",
+        "boxes__2__width",
+        "product__styles"]
+    return columns
+
+
+country_mapping = {
+    'China': 'CHN',
+    'India': 'IND',
+    'Indonesia': 'IDN',
+    'Phillipines': 'PH',
+    'Thailand': 'TH',
+    'Vietnam': 'VN'
+}
